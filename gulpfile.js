b232c00c60bd9ae7bf190d8ce5f74e60bf8e5b87
Elena Kuznetsova <kuzn.elena@yahoo.com>
2023-04-07 11:30:27 +0500

    fix link

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ export const css = () => gulp
     .pipe(browserSync.stream());
 
 export const js = () => gulp
-    .src('src/**/*.js')
+    .src('src/js/**/*.js')
     .pipe(gulp.dest('dist/js'))
     .pipe(browserSync.stream());
 
